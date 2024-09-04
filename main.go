8bafb318722b91fa7a3943d521d47261e0290e7b
Razhok Aliaksandr <aliaksandr.razhok.std@gmail.com>
2024-09-04 20:42:35 +0400

    create postgres database and endpoint for creating user

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"blog_aggregator/internal/database"
 	"blog_aggregator/models"
 	"blog_aggregator/tools"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -18,10 +25,17 @@ func main() {
 
 	mux := http.NewServeMux()
 	port := os.Getenv("PORT")
+	dbURL := os.Getenv("CONNECTION_STRING")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
 
 	server := &http.Server{
 		Addr:    port,
 		Handler: mux,
+	}
+
+	config := models.ApiConfig{
+		DB: dbQueries,
 	}
 
 	mux.HandleFunc("GET /v1/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +48,42 @@ func main() {
 	mux.HandleFunc("GET /v1/err", func(w http.ResponseWriter, r *http.Request) {
 		tools.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
 	})
+	mux.HandleFunc("POST /v1/users", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			return
+		}
+
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Printf("Error writing response: %v\n", err)
+			}
+		}(r.Body)
+
+		var userData models.User
+		if err := json.Unmarshal(bodyBytes, &userData); err != nil {
+			fmt.Printf("Error unmarshalling body: %v\n", err)
+		}
+
+		if userData.Name == "" {
+			fmt.Errorf("Name can't be blank")
+		}
+
+		user, err := config.DB.CreateUser(ctx, database.CreateUserParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+			Name:      userData.Name,
+		})
+		if err != nil {
+			fmt.Printf("Error creating user: %v\n", err)
+		}
+
+		tools.RespondWithJSON(w, http.StatusOK, user)
+	})
 
 	err = http.ListenAndServe(server.Addr, server.Handler)
 	if err != nil {
@@ -41,4 +91,5 @@ func main() {
 	} else {
 		fmt.Println("Server is running")
 	}
+
 }
--- models/models.go
+++ models/models.go
@@ -1,5 +1,22 @@
 package models
 
+import (
+	"blog_aggregator/internal/database"
+	"github.com/google/uuid"
+	"time"
+)
+
 type Status struct {
 	Status string `json:"status"`
+}
+
+type ApiConfig struct {
+	DB *database.Queries
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
 }
